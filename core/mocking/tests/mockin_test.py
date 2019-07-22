32799eda63aa61b4739631d39bae15fc7c400ce3
Josheriff <jakermater@gmail.com>
2019-07-22 09:25:39 +0200

    sut is the mock, but to show how to fake, good enough right now

--- core/mocking/tests/mockin_test.py
+++ core/mocking/tests/mockin_test.py
@@ -1,10 +1,19 @@
 import pytest
 from unittest.mock import MagicMock
 import requests
+import json
 
 from core.mocking.mockin import get_pokemons_number
 
 POKEMONS_NUMBER = 964 # In the moment of this test are 964 pokemons in the api
+
+class MockResponse(object):
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
 
 def test_get_pokemons_number():
     pokemons_number = get_pokemons_number(requests)
@@ -33,3 +42,24 @@ def test_collaboration_called_once():
     get_pokemons_number(request_mock)
 
     assert request_mock.get.called_once()
+
+#
+#   IMPORTANT NOTE:
+#
+#   Here we are asserting that our mock method is what we expect
+#   OF COURSE IT IS, thats why we are mocking it.
+#   THIS TEST it's only to whow how to "fake" a request answer
+#   Usually you will have to do something with that answer, and that is:
+#   WHAT YOU HAVE TO TEST == SUT (subject under test)
+#
+#   SUT never can be a MOCK.
+#
+
+def test_collaboration_with_return():
+    request_mock = MagicMock()
+    pokemon_json = MockResponse({'count':1}, 200)
+    request_mock.get.return_value = pokemon_json
+
+    mocked_result = get_pokemons_number(request_mock)
+
+    assert mocked_result == 1
